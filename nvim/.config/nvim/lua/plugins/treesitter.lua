43c3c352c887bb1020b051ecddeaf80f45cba9d5
Daniel Mamede <mamededaniel01@gmail.com>
2026-07-05 19:15:47 -0300

    update 2026-07-05 19:15

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,12 +3,16 @@ return {
   lazy = false,
   build = ':TSUpdate',
   config = function()
-    require("nvim-treesitter").setup({
-      ensure_installed = { "python", "html", "css", "javascript" },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
+    local languages = { "python", "html", "css", "javascript", "java" }
+
+    require("nvim-treesitter").setup()
+    require("nvim-treesitter").install(languages)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = languages,
+      callback = function()
+        vim.treesitter.start()
+      end,
     })
   end,
 }
